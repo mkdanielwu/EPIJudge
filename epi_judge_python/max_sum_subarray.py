1edb71250669a6fe9798543a9f0e03d44dfb3933
mwu5 <daniel_wu@intuit.com>
2020-03-24 16:21:49 -0700

    additional work

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -4,8 +4,21 @@ from test_framework import generic_test
 
 
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+    max_seen, current_max = A[0], A[0]
+    max_seen_left, max_seen_right = 0, 0
+    max_current_left, max_current_right = 0, 0
+    for i in range(1, len(A)):
+        if current_max + A[i] >= A[i]:
+            current_max = current_max + A[i]
+            max_current_right = i
+        else:
+            current_max = A[i]
+            max_current_left = max_current_right = i
+        if max_seen < current_max:
+            max_seen = current_max
+            max_seen_left, max_seen_right = max_current_left, max_current_right
+    print("max={}, range=[{},{}]".format(max_seen, max_seen_left, max_seen_right))
+    return max_seen
 
 
 if __name__ == '__main__':
--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -3,9 +3,33 @@ from typing import List
 from test_framework import generic_test
 
 
+# original: space complexity: O(sn)
+def num_combinations_for_final_score1(final_score: int,
+                                     individual_play_scores: List[int]) -> int:
+    score_combo = [[1] + [0]*final_score for _ in individual_play_scores]
+    for i, r in enumerate(individual_play_scores):
+        for c in range(1, final_score+1):
+            without_this_play = score_combo[i-1][c] if i > 0 else 0
+            with_this_play = score_combo[i][c - r] if c >= r else 0
+            score_combo[i][c] = without_this_play + with_this_play
+    return score_combo[-1][-1]
+
+
+# variant 1: space complexity: O(s)
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
+    score_combo = [1] + [0]*final_score
+    for i, r in enumerate(individual_play_scores):
+        for c in range(1, final_score+1):
+            without_this_play = score_combo[c] if i > 0 else 0
+            with_this_play = score_combo[c - r] if c >= r else 0
+            score_combo[c] = without_this_play + with_this_play
+    return score_combo[-1]
+
+
+# variant 2: return number of sequences (permutations)
+def num_sequences_for_final_score(final_score: int,
+                                  individual_play_scores: List[int]) -> int:
     return 0
 
 
